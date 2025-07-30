f5759e286ab532b3a60182809cd45a18b0ac15e9
Rhythm Dubey <rhythm@itradicals.com>
2025-07-30 12:32:30 +0530

    List rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { useState } from 'react';
 import Welcome from './components/Welcome'
 
 function App() {
-  const isLoggedIn = false;
+  const projects = ['My Rental Spot', 'Rentler', 'Business My Rental Spot'];
 
   return (
     <div>
-      <h2>{isLoggedIn ? "Welcome back!" : "Please login."}</h2>
+      <h2>Porjects List</h2>
+      <ul>
+        {
+          projects.map((project, index) => (
+            <li key={index}>{project}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
