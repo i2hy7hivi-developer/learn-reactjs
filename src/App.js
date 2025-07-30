79ea8c3dda794e7865029c1b955ba66c5e9ba23b
Rhythm Dubey <rhythm@itradicals.com>
2025-07-30 12:22:55 +0530

    Simple useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import Welcome from './components/Welcome'
 
 function App() {
+  const [count, setCount] = useState(69);
+
   return (
-    <>
-      <Welcome name="Rhythm" />
-      <Welcome name="Sunil" />
-      <Welcome name="Prkash" />
-      <Welcome name="Dinesh" />
-    </>
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 10)}>+ Increment</button>
+      <button onClick={() => setCount(count - 1)}>- Decrement</button>
+    </div>
   );
 }
 
