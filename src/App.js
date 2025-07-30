f768c3fc1004b7b3f5d1376c9c7c0c087a4ba1f4
Rhythm Dubey <rhythm@itradicals.com>
2025-07-30 13:00:55 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { useEffect, useState } from 'react';
-import Welcome from './components/Welcome'
+import Welcome from './components/Welcome';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home()
+{
+  return <div>Home Page</div>
+}
+
+function About()
+{
+  return <div>About Page</div>
+}
+
+function ContactUs()
+{
+  return <div>ContactUs Page</div>
+}
 
 function App() {
-  useEffect(() => {
-    console.log('Component loaded.')
-  });
-
   return (
-    <div>
-      Check the console.
-    </div>
-  );
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/about">About</Link> |
+        <Link to="/contact-us">Contact Us</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
